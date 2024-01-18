6b13ff90acb58b644d537e43388874b605021aec
IsMaiL Labiade <59127571+IsMaiLLaBiaDe@users.noreply.github.com>
2024-01-18 22:48:04 +0100

    Add files via upload

---
+++ kr/history.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head></head>
+<body>
+<style></style>
+</body>
+<audio src="history.mp3"></audio>
+<script>window.addEventListener("DOMContentLoaded", event => {
+  const audio = document.querySelector("audio");
+  audio.volume = 0.2;
+  audio.play();
+});
+</script>
+</html>
