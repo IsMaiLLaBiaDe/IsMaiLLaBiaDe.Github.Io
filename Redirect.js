51f848a2dd3d9fc4a8c2325f230c730f60063fd9
IsMaiL LaBiaDe <59127571+IsMaiLLaBiaDe@users.noreply.github.com>
2024-07-03 13:51:52 +0100

    Update Redirect.js

--- Redirect.js
+++ Redirect.js
@@ -5,5 +5,5 @@
         window.location="https://www.google.com/search?q=IsMaiL+LaBiaDe"; 
     } 
     document.write("You will be redirected to a new page in 5 seconds"); 
-    setTimeout('Redirect()', 9000);   
+    setTimeout('Redirect()', 1000);   
 </script>
