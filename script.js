ab39ea9ae311a01b32ab54c170c5c775f464e59d
IsMaiL LaBiaDe <59127571+IsMaiLLaBiaDe@users.noreply.github.com>
2025-08-27 22:48:47 +0100

    Update script.js

--- script.js
+++ script.js
@@ -18,9 +18,9 @@ setInterval(() => {
  const secondes = Math.floor((tempsRestant % (1000 * 60)) / 1000);
 
  // Afficher le countdown
- countdownElement.textContent = `Temps restant : ${jours} jours, ${heures} heures, ${minutes} minutes et ${secondes} secondes`;
-}, 1000);
-countdown_Element.textContent = `Temps restant : ${jours} jours, ${heures} heures, ${minutes} minutes et ${secondes} secondes`;
-}, 1000);
+ countdown_Element.textContent = `Temps restant : ${jours} jours, ${heures} heures, ${minutes} minutes et ${secondes} secondes`;
+ countdownElement.textContent = `Temps restant : ${jours} jours, ${heures} heures, ${minutes} minutes et ${secondes} secondes`; }, 1000);
+
+
 
 
