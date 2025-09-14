ad22623a515a3336d7010b9d75153ca0659997d2
IsMaiL LaBiaDe <59127571+IsMaiLLaBiaDe@users.noreply.github.com>
2025-09-14 17:45:37 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
 // Définir la date cible
 const dateCible = new Date('2026-03-25 00:00:00'); // Exemple : 31 décembre 2024, 23h59min59s
-
+const date_Cible = new Date('2101-03-25 00:00:00'); // Exemple : 31 décembre 2024, 23h59min59s
 
 // Sélectionner l'élément HTML
 const countdownElement = document.getElementById('countdown');
-
- const date_Cible = new Date('2101-03-25 00:00:00'); // Exemple : 31 décembre 2024, 23h59min59s
 const countdown_Element = document.getElementById('count_down');
 
 // Mettre à jour le countdown toutes les secondes
 setInterval(() => {
  const maintenant = new Date();
+ const maint_enant = new Date();
  const tempsRestant = dateCible.getTime() - maintenant.getTime();
-  const maint_enant = new Date();
  const temps_Restant = date_Cible.getTime() - maint_enant.getTime();
 
  // Calculer les jours, heures, minutes et secondes restantes
@@ -21,10 +19,17 @@ setInterval(() => {
  const minutes = Math.floor((tempsRestant % (1000 * 60 * 60)) / (1000 * 60));
  const secondes = Math.floor((tempsRestant % (1000 * 60)) / 1000);
 
+  // Calculer les jours, heures, minutes et secondes restantes
+ const j_ours = Math.floor(temps_Restant / (1000 * 60 * 60 * 24));
+ const h_eures = Math.floor((temps_Restant % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+ const m_inutes = Math.floor((temps_Restant % (1000 * 60 * 60)) / (1000 * 60));
+ const s_econdes = Math.floor((temps_Restant % (1000 * 60)) / 1000);
 
  // Afficher le countdown
- countdown_Element.textContent = `Temps restant : ${j_ours} jours, ${h_eures} heures, ${m_inutes} minutes et ${s_econdes} secondes`;
+ countdown_Element.textContent = `Temps restant : ${jours} jours, ${heures} heures, ${minutes} minutes et ${secondes} secondes`;
  countdownElement.textContent = `Temps restant : ${jours} jours, ${heures} heures, ${minutes} minutes et ${secondes} secondes`; }, 1000);
+
+
 
 
 
